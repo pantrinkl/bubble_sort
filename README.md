5f2cef4340a441bd8833be857564a2a0e9e5efe9
Filip Raasch <raasch.f@gmail.com>
2021-02-04 22:57:57 +0100

    zapracovani issues

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # Bubble sort
-Tento program slouží k setřídění posloupnosti čísel od největšího po nejmenší, a to pomocí metody bubble sort.
+Tento program slouží k setřídění posloupnosti čísel sestupně, či vzestupně, a to pomocí metody bubble sort.
 
 Setříděná posloupnost čísel je následně uložena do textového souboru.
 
 ## Vstupní data 
-Ve složce, kde je uložen `.py` soubor, je nutné mít soubor:
-* `vstup.txt`\
-V souboru je třeba mít čísla, která mají být setříděná.\
+Po spuštění programu je nutné zadat dva parametry:
+* Jméno vstupního souboru.\
+V textovém souboru je třeba mít čísla, která mají být setříděná.\
 Čísla mohou být oddělená mezerou nebo čárkou.\
 Tečka je brána jako desetinná značka.\
 Další znaky nebo slova jsou ignorovány.
+* Zda má být posloupnost setříděna vzestupně (asc) nebo sestupně (desc).
 
 ## Výstupní data
 Setříděná posloupnost čísel je uložena do souboru `vystup.txt`. Pokud se ve složce tento souboru nevyskytuje, je vytvořen nový. V případě, že ve složce již takový soubor existuje, je přepsán.
 
 Podrobné popsání tohoto programu je v souboru `dokumentace.pdf`.
 
-Pro vyzkoušení programu můžete využít z tohoto repozitáře testovací soubor `vstup.txt`. Výsledek setříděné posloupnosti naleznete v testovacím souboru `vystup.txt`.
+Pro vyzkoušení programu můžete využít z tohoto repozitáře testovací soubor `vstup.txt`. Výsledek sestupně setříděné posloupnosti naleznete v testovacím souboru `vystup.txt`.
--- bubble_sort.py
+++ bubble_sort.py
@@ -1,55 +1,82 @@
-# definovani funkci
+# defining functions
 
-# zjisteni, zda se jedna o cislo nebo o text
-def zda_cislo(x):
+# decide whether number or text
+def is_number(x):
     try:
         float(x)
         return True
     except ValueError:
         return False
 
-# nacteni dat ze souboru vstup.txt
-def nacist_cisla():
+# load input data
+def load_numbers(file_name):
     try:
-        with open('vstup.txt', 'r') as h:
-            obsah = h.readlines() 
-        sez_cisla = []
-        for radek in obsah:
-            # pokud je oddelovac carka, vymenime ji za mezeru
-            radek.replace(',',' ')
-            # rozdelime radek na casti podle mezer
-            for i in radek.split(): 
-                # pokud polozka v radku je cislo, pridame do seznamu cisel
-                if zda_cislo(i) == True: 
-                    sez_cisla.append(float(i))
-        return sez_cisla
+        with open(file_name, 'r') as h:
+            content = h.readlines() 
+        list_nums = []
+        for line in content:
+            # if delimiter is comma, replaced with space
+            line.replace(',',' ')
+            # divide line to parts by spaces
+            for i in line.split(): 
+                # if item in line is a number, add to list of numbers
+                if is_number(i) == True: 
+                    list_nums.append(float(i))
+        return list_nums
     except FileNotFoundError:
-        exit("Nenalezen vstupní soubor vstup.txt")
+        exit("Nenalezen vstupní soubor.")
     except PermissionError:
-        exit("Nemám přístup k vstupnímu souboru vstup.txt")
+        exit("Nemám přístup k vstupnímu souboru.")
 
-# ulozeni setridenych cisel do vystup.txt
-def uloz_cisla(setridena):
+# save sorted numbers to file: vystup.txt
+def save_nums(sorted_nums):
     try:
         with open('vystup.txt','w') as k:
-            for i in setridena:
+            for i in sorted_nums:
                 k.write("{:g} ".format(i))
     except PermissionError:
         exit("Nemám přístup k výstupnímu souboru vystup.txt")
 
-# hlavni cast programu
+# main program
 
-# nacteni dat
-cisla = nacist_cisla()
-# setrideni cisel metodou bubble sort
-p=0
-while p == 0:
-    p = 1
-    for i in range(len(cisla)-1):
-        if cisla[i]<cisla[i+1]:
-            a=cisla[i]
-            cisla[i]=cisla[i+1]
-            cisla[i+1]=a
-            p=0
-# export setridenych cisel
-uloz_cisla(cisla)
+# get name of input file from user
+input_name = str(input("Zadejte název vstupního souboru: "))
+
+# get ascending or descending order
+p=1
+while p==1: 
+    order = str(input("Zadejte zda vzestupné(asc) nebo sestupné(desc) třídění: "))
+    if order != "desc" and order != "asc":
+        print("Špatně zadané desc nebo asc.")
+    else:
+        p+=1
+
+# load numbers from 
+num_list = load_numbers(input_name)
+
+# sort loaded numbers with bubble sort method - ascending
+if order == "asc":
+    p=0
+    while p == 0:
+        p = 1
+        for i in range(len(num_list)-1):
+            if num_list[i]>num_list[i+1]:
+                a=num_list[i]
+                num_list[i]=num_list[i+1]
+                num_list[i+1]=a
+                p=0
+
+# sort loaded numbers with bubble sort method - descending      
+if order == "desc":
+    p=0
+    while p == 0:
+        p = 1
+        for i in range(len(num_list)-1):
+            if num_list[i]<num_list[i+1]:
+                a=num_list[i]
+                num_list[i]=num_list[i+1]
+                num_list[i+1]=a
+                p=0
+                
+# save sorted numbers
+save_nums(num_list)
